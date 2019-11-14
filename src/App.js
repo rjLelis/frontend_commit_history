fb26e517ef834370fdcbc7154b992b86c45715be
rjLelis <renatojlelis@gmail.com>
2019-11-14 18:04:30 -0300

    Integrate repository api

--- src/App.js
+++ src/App.js
@@ -3,17 +3,36 @@ import Header from './components/Header';
 import RepositoryList from './components/RepositoryList';
 import CommitList from './components/CommitList';
 import './Main.css';
+import instance from './api';
 
-function App() {
-  return (
-    <div className="padding-10">
-      <Header />
-      <main>
-        <RepositoryList />
-        <CommitList />
-      </main>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        repositories: [],
+      };
+    }
+
+    async componentDidMount() {
+      const response = await instance.get('/repositories/api');
+      const { results } = response.data;
+      this.setState({ repositories: results })
+    }
+
+
+
+    render(){
+      const { repositories } = this.state;
+      return (
+        <div className="padding-10">
+          <Header />
+          <main>
+            <RepositoryList repositories={repositories}/>
+            <CommitList />
+          </main>
+        </div>
+      );
+    }
 }
-
 export default App;
