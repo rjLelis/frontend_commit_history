96797a93b89b524d445e42926064f28d948f780b
rjLelis <renatojlelis@gmail.com>
2019-11-19 10:03:46 -0300

    Interate over repository list to create the card

--- src/components/RepositoryList.js
+++ src/components/RepositoryList.js
@@ -1,25 +1,39 @@
 import React from 'react';
 
-
 class RepositoryList extends React.Component {
 
-    render() {
-        const {repositories}  = this.props;
-        console.log(repositories)
+    renderRepos() {
+        const { repositories } = this.props;
         return (
-             repositories.map(repository => {
-                return (
-                    <section key={repository.id} id="repository-list" className="margin-30">
-                        <div className="repository padding-10-15">
-                            <p>repository.name</p>
-                            <small>repository.description</small>
+            <section id="repository-list" className="margin-30">
+                {repositories.map(repository => {
+                    return (
+                        <div key={repository.id}
+                            className="repository padding-10-15"
+                            onClick={() => this.props.onClick(repository.name)}
+                        >
+                            <p>{repository.name}</p>
+                            <small>{repository.description}</small>
                         </div>
-                    </section>
-                )
-            })
+                    )
+                })}
+            </section>
         );
     }
 
+    render() {
+        return (
+            this.props.repositories.length > 0 ? (
+                this.renderRepos()
+            ) : (
+            <section id="repository-list" className="margin-30">
+                <div className="repository padding-10-15">
+                    <p>No repositories to show</p>
+                </div>
+            </section>
+            )
+        );
+    }
 }
 
 export default RepositoryList;
