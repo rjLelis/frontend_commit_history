964d534f7a9dde9a9bb10b85b937a8f4d284cc8f
rjLelis <renatojlelis@gmail.com>
2019-11-19 10:02:56 -0300

    Interate over commit list to create the card

--- src/components/CommitList.js
+++ src/components/CommitList.js
@@ -1,28 +1,46 @@
 import React from 'react';
+import Moment from 'react-moment';
 
-function CommitList() {
-    return (
-        <section id="commit-list">
-            <div className="commit padding-10-15">
-                <p>commit e43c80860d59751807b79af89ff390cd262ae6f48b7b1493</p>
-                <p>Author: rjLelis &lt;renatojlelis@gmail.com&gt;</p>
-                <p>Date: Thu Nov 7 15:33:07 2019 -0300</p>
-                <p> Firt Commit</p>
-            </div>
-            <div className="commit padding-10-15">
-                <p>commit e43c80860d59751807b79af89ff390cd262ae6f48b7b1493</p>
-                <p>Author: rjLelis &lt;renatojlelis@gmail.com&gt;</p>
-                <p>Date: Thu Nov 7 15:33:07 2019 -0300</p>
-                <p> Firt Commit</p>
-            </div>
-            <div className="commit padding-10-15">
-                <p>commit e43c80860d59751807b79af89ff390cd262ae6f48b7b1493</p>
-                <p>Author: rjLelis &lt;renatojlelis@gmail.com&gt;</p>
-                <p>Date: Thu Nov 7 15:33:07 2019 -0300</p>
-                <p> Firt Commit</p>
-            </div>
-        </section>
-    );
+class CommitList extends React.Component {
+
+    renderCommits() {
+        const { commits } = this.props;
+        return (
+            <section id="commit-list">
+                {commits.map(commit => {
+                    return (
+                        <div
+                            key={commit.node_id}
+                            className="commit padding-10-15"
+                        >
+                            <p>commit {commit.sha}</p>
+                            <p>
+                                Author: {commit.commit.author.name}
+                                &lt;{commit.commit.author.email}&gt;
+                            </p>
+                            Date: <Moment>{commit.commit.author.date}</Moment>
+                            <p>{commit.commit.message}</p>
+                        </div>
+                    )
+                })}
+            </section>
+        );
+    }
+
+    render(){
+        return (
+            this.props.commits.length > 0 ? (
+                this.renderCommits()
+            ) : (
+            <section id="commit-list">
+                <div className="commit padding-10-15">
+                   <p>Select a repository to visualize it's commits</p>
+                </div>
+            </section>
+            )
+
+        );
+    }
 }
 
 export default CommitList;
