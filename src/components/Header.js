5833d91784f99a49b54f736a71178ab0c23811ae
rjLelis <renatojlelis@gmail.com>
2019-11-19 10:04:56 -0300

    Integrate repository form

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,39 @@ import './../Main.css';
 
 class Header extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {repoName: ""};
+    }
+
+    handleChange = e => {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    handleSumit = e => {
+        const { repoName } = this.state;
+        this.props.onSubmit(e, repoName);
+        this.setState({repoName: ""});
+    }
+
     render(){
         return (
             <header className="padding-10">
-                <form>
+                <form method="post">
+                    <input
+                        type="text"
+                        id="repo-name"
+                        placeholder="Type the name of one of yours repository"
+                        onChange={this.handleChange}
+                        value={this.state.RepoName}
+                        name="repoName"
+                    />
 
-                    <input type="text"
-                    name="repo-name" id="repo-name"
-                    placeholder="Digite o nome de um repositório"/>
-
-                    <button type="submit">Add</button>
+                    <button
+                        onClick={this.handleSumit}
+                    >
+                        Add
+                    </button>
                 </form>
             </header>
         );
