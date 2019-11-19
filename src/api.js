6c50e10c4817c0f0039200d28491d27803345f6e
rjLelis <renatojlelis@gmail.com>
2019-11-19 10:00:51 -0300

    Create base url for github's API

--- src/api.js
+++ src/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
-const instance = axios.create({
-    'baseURL': 'http://localhost:8000',
+const backendAPI = axios.create({
+    'baseURL': 'http://localhost:8000/api',
 });
 
-export default instance;
+const githubAPI = axios.create({
+    'baseURL': 'https://api.github.com'
+})
+export { backendAPI, githubAPI };
